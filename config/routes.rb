49ba09387853b56abb4aa1aca8345537233c659b
moranja <jeffadammoran@gmail.com>
2019-06-11 16:31:03 -0500

    routes refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,19 @@
 Rails.application.routes.draw do
-  resources :attractions, constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  resources :areas, constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  resources :cities, constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  resources :plans, constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  resources :itineraries, constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  resources :user_itineraries, constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  resources :users, constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
 
-  post '/login', to: 'users#authenticate', constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  post '/copyItinerary', to: 'itineraries#copy', constraints: ->(request) do
-    request.xhr? && !request.format.html?
-  end
-  post '/itineraries/:id/nearest', to: 'itineraries#nearest', constraints: ->(request) do
-    request.xhr? && !request.format.html?
+  constraints( |request| request.xhr? && !request.format.html? ) do
+    resources :attractions
+    resources :areas
+    resources :cities
+    resources :plans
+    resources :itineraries#, constraints: ->(request) do
+    #   request.xhr? && !request.format.html?
+    # end
+    resources :user_itineraries
+    resources :users
+
+    post '/login', to: 'users#authenticate'
+    post '/copyItinerary', to: 'itineraries#copy'
+    post '/itineraries/:id/nearest', to: 'itineraries#nearest'
   end
 
   get '*path', to: "application#react_app", constraints: ->(request) do
